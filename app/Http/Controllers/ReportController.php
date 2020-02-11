c73559d36b97814539234ff5fdb72511a41fdb8f
Aye-Lwin-Soe <ayelwinsoe.it2018@gmail.com>
2020-02-11 13:31:52 +0630

    Monthly Report

---
+++ app/Http/Controllers/ReportController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Carbon\Carbon;
+use DB;
+class ReportController extends Controller
+{
+    //
+    public function report(){
+        $dateY = date('Y') - 4; 
+       //dd($dateY);
+        $years = range(Carbon::now()->year, $dateY);
+       /*dd($years);
+       
+       dd($dateY);*/
+
+      
+       return view('monthlyreports.report',compact('years'));
+    }
+
+    public function detailsearch(Request $request)
+    {
+        if(request('month') && request('year') ){
+         $month = $request->month;
+         $year = $request->year;
+
+        switch ($month) {
+            case 'Jan':
+                $month='01';
+                break;
+
+            case 'Feb':
+                $month='02';
+                break;
+            
+            case 'Mar':
+                $month='03';
+                break;
+
+            case 'April':
+                $month='04';
+                break;
+
+            case 'May':
+                $month='05';
+                break;
+
+            case 'June':
+                $month='06';
+                break;
+
+            case 'July':
+                $month='07';
+                break;
+
+            case 'Aug':
+                $month='08';
+                break;
+
+            case 'Sep':
+                $month='09';
+                break;
+
+            case 'Oct':
+                $month='10';
+                break;
+
+            case 'Nov':
+                $month='11';
+                break;
+
+            case 'Dec':
+                $month='12';
+                break;
+            default:
+                
+                break;
+        }
+        //dd($month);
+        $startdate = $year.'-'.$month.'-01';
+        //dd($startdate);
+        $enddate = $year.'-'.$month.'-31';
+        //dd($enddate);
+        $result = DB::table('expenses')
+                    ->join('users','users.id','=','expenses.user_id')
+                    ->select('expenses.*','users.name as uname')
+                    ->whereBetween('date',[$startdate,$enddate])
+                    ->get();
+                    //dd($result);
+        $iresult = DB::table('incomes')
+                    ->join('users','users.id','=','incomes.user_id')
+                    ->select('incomes.*','users.name as iuname')
+                    ->whereBetween('date',[$startdate,$enddate])
+                    ->get();
+                    //dd($iresult);
+                    /*dd($result);*/
+         return response()->json([
+            "result"=>$result,
+            "iresult"=>$iresult
+        ],200);
+     }
+    }
+}
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,4 +2,70 @@
 @section('content')
   <!-- Page Heading -->
   <h1 class="h3 mb-4 text-gray-800">Dashboard Page</h1>
+  <div class="row">
+        <div class="col-md-4">
+            <a href="report" class="nav-link">
+                <div class="card mb-3 shadow mx-3 mycard">
+                    <div class="row no-gutters">
+                        <div class="col-md-4">
+                          <img src="{{url('/img/profit.png')}}" class="card-img mt-3 p-3" alt="...">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">Income / Expense</h5>
+                                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="col-md-4">
+            <a href="student" class="nav-link">
+                <div class="card mb-3 shadow mx-3 mycard">
+                    <div class="row no-gutters">
+                        <div class="col-md-4">
+                          <img src="{{asset('/img/student.png')}}" class="card-img mt-3 p-3" alt="...">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">Students</h5>
+                                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="col-md-4">
+            <a href="teacher" class="nav-link">
+                <div class="card mb-3 shadow mx-3 mycard">
+                    <div class="row no-gutters">
+                        <div class="col-md-4">
+                          <img src="{{asset('/img/classroom.png')}}" class="card-img mt-3 p-3" alt="...">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">Teachers</h5>
+                                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </div>
+    </div>
 @endsection
+
+<style type="text/css">
+    .mycard{
+        border-radius: 0;
+        transition: 0.9s;
+    }
+
+    .mycard:hover{
+        cursor: pointer;
+        transition: 1s;
+        transform: scale(1.1);
+    }
+</style>
