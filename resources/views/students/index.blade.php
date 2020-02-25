e9f63f93674516efd32a9fcbb42fc7bf94cf2b0a
yathaw <yathawmyatnoe007@gmail.com>
2020-02-25 13:31:29 +0630

    Student Detail

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -14,6 +14,7 @@
       <div class="form-group col-md-4">
         <label for="inputCourse">Choose Course:</label>
         <select name="course" class="form-control" id="course">
+          <option> Choose Course </option>
           @foreach($courses as $row)
           <option value="{{$row->id}}">{{$row->name}}</option>
           @endforeach
@@ -23,21 +24,23 @@
       <div class="form-group col-md-4">
         <label for="inputCourse">Choose Batch:</label>
         <select name="batch" class="form-control" disabled="disabled" id="batch">
-          @foreach($batches as $row)
+          <option> Choose Batch </option>
+          {{-- @foreach($batches as $row)
           <option value="{{$row->id}}">{{$row->title}}</option>
-          @endforeach
+          @endforeach --}}
         </select>
       </div>
 
       <div class="form-group col-md-2 mt-2">
         <button type="submit" class="btn btn-primary mt-4">Search</button>
       </div>
-
+      
+      @if($bid !=0)
       <div class="form-group col-md-2 text-right mt-2">
 
-        <a name="btnSelect" href="{{route('export',$row->id)}}" role="button" class="btn btn-info mt-4"><i class="fas fa-upload fa-sm"></i> Generate Excel</a>
-    
+        <a name="btnSelect" href="{{route('export',$row->id)}}" role="button" class="btn btn-info mt-4"><i class="far fa-file-excel"></i> Print Attendance </a>
       </div>
+      @endif
     </div>
   </form>
 
@@ -92,47 +95,6 @@
     @else
       <h2>Please, Create Group!!!</h2>
     @endif
-  @else
-    <table class="table table-bordered my-3">
-      <thead>
-        <tr>
-          <th>No</th>
-          <th>Name</th>
-          <th>Email</th>
-          <th>Phone No</th>
-          <th>Batch</th>
-          <th>Action</th>
-        </tr>
-      </thead>
-      <tbody>
-        @php
-          $i = 1;
-        @endphp
-        @foreach($students as $row)
-        <tr>
-          <td>{{$i++}}</td>
-          <td>{{$row->namee}}</td>
-          <td>{{$row->email}}</td>
-          <td>{{$row->phone}}</td>
-          <td>{{$row->batch->course->name}} - {{$row->batch->title}}</td>
-          <td>
-            <a href="{{route('students.show',$row->id)}}" class="btn btn-primary"><i class="fas fa-info"></i></a>
-            <a href="{{route('students.edit',$row->id)}}" class="btn btn-warning"><i class="fas fa-edit"></i></a>
-           
-            <form method="post" action="{{route('students.destroy',$row->id)}}" class="d-inline-block">
-              @csrf
-              @method('DELETE')
-              {{-- @if($row->trashed())
-                <button type="submit" class="btn btn-danger">Restore</button>
-              @else --}}
-                <button type="submit" class="btn btn-danger"><i class="fas fa-trash"></i></button>
-              {{-- @endif --}}
-            </form>
-          </td>
-        </tr>
-        @endforeach
-      </tbody>
-    </table>
   @endif
 @endsection
 
