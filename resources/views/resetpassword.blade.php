3c987678e9eee88fda8202c7da771ffef6a055bd
thetpainghtut <thetpainghtut.bf@gmail.com>
2020-10-13 16:42:48 +0630

    access app url from environment

--- resources/views/resetpassword.blade.php
+++ resources/views/resetpassword.blade.php
@@ -21,7 +21,7 @@
 
 				<p>Hello!</p>
 				<p>You are receiving this email because we recived a password reset for your account.</p>
-				<a href="{{App::environment('APP_URL')}}/resetandeditpassword/?codeno={{$data['codeno']}}&email={{$data['email']}}">
+				<a href="{{ env('APP_URL') }}/resetandeditpassword/?codeno={{$data['codeno']}}&email={{$data['email']}}">
 					@csrf
 					<button type="submit" class="btn btn-outline-warning" style="color: white; padding: 10px;border: none; background-color: #29A3F7;border-radius: 5px">
 				       	Reset Password
